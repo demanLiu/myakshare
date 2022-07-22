6a314690937954e0cb4631bf3ab4801332c653a2
demanLiu <qq384012351@gmail.com>
2022-07-22 13:09:36 +0800

    update index

--- data/sh000001.py
+++ data/sh000001.py
@@ -1,23 +1,33 @@
 
+import sys
+sys.path.append(".")
+import utils.mongodbUtil as mongodbUtil
 import akshare as ak
-import pymongo
-import time
 from pymongo import UpdateOne
-result = ak.stock_zh_index_daily(symbol="sh000001")
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["stock"]
-historyCol = db["history"]
-codeItem={}
-codeItem["code"]="000001"
-codeItem["region"]="sh"
-updateOp=[]
-for index, row in result.iterrows():
-    myquery = {"code": codeItem["code"],"region": codeItem["region"], "date": str(index).split()[0]}
-    valueItem={}
-    for columnItem in result.columns:
-        valueItem[columnItem] = row[columnItem]
-    valueItem["code"]=codeItem["code"]
-    op = UpdateOne(myquery, {'$set': valueItem}, upsert=True)
-    updateOp.append(op)
-if len(updateOp)>0:
-    bulkRes = historyCol.bulk_write(updateOp)
+
+historyCol = mongodbUtil.getCol("stock", "importIndex")
+# 
+
+
+def writeMongo(code, region, data):
+    codeItem = {}
+    codeItem["code"] = code
+    codeItem["region"] = region
+    updateOp = []
+    for index, row in data.iterrows():
+        myquery = {
+            "code": codeItem["code"], "region": codeItem["region"], "date":str(row["date"])}
+        valueItem = {}
+        for columnItem in data.columns:
+            valueItem[columnItem] = row[columnItem]
+        valueItem["code"] = codeItem["code"]
+        valueItem["date"] = str(row["date"])
+        op = UpdateOne(myquery, {'$setOnInsert': valueItem}, upsert=True)
+        updateOp.append(op)
+    if len(updateOp) > 0:
+        historyCol.bulk_write(updateOp)
+
+
+writeMongo("000001","sh",ak.stock_zh_index_daily(symbol="sh000001"))
+writeMongo("399001","sz",ak.stock_zh_index_daily(symbol="sz399001"))
+writeMongo("399006","sz",ak.stock_zh_index_daily(symbol="sz399006"))
