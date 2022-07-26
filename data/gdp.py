fc98dbe871594ee78ca2ff1ca99a538f9cf380dc
demanLiu <qq384012351@gmail.com>
2022-07-26 23:41:52 +0800

    add gdp cur quarter value

--- data/gdp.py
+++ data/gdp.py
@@ -26,3 +26,29 @@ for index,row in  gdp.iterrows():
     updateOp.append(op)
 
 col.bulk_write(updateOp)
+def  getQuarterValue(year):
+    cursor = col.find({"date":{"$gt":str(year), "$lt":str(year+1)}}).sort("date",1)
+    data=[]
+    for gdpItem in cursor:
+        data.append(gdpItem)
+    length = len(data)
+    if length==0:
+        return
+    updateOp=[]
+    data[0]["curGdp"]=data[0]['gdp']
+    myquery = {"date": data[0]['date'] }
+    op = UpdateOne(myquery, {'$set': data[0]}, upsert=True)
+    updateOp.append(op)
+    if length==1:
+        col.bulk_write(updateOp)
+        return 
+    for i in range(1,length):
+        data[i]["curGdp"]=data[i]['gdp']-data[i-1]['gdp']
+        myquery = {"date": data[i]['date'] }
+        op = UpdateOne(myquery, {'$set': data[i]}, upsert=True)
+        updateOp.append(op)
+
+    col.bulk_write(updateOp)
+    print(updateOp)
+for year in range(2022,2023):
+    getQuarterValue(year)
