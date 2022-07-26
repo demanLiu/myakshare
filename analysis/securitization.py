022dd801973fd66adb41e70a51e5923dd0c6e82b
demanLiu <qq384012351@gmail.com>
2022-07-26 23:02:42 +0800

    add securitization

--- analysis/securitization.py
+++ analysis/securitization.py
@@ -1,9 +1,17 @@
 import sys
 sys.path.append(".")
 import utils.mongodbUtil as mongodbUtil
-
+from  datetime import datetime
 gdpCol = mongodbUtil.getCol("stock","gdp")
 dailyCol = mongodbUtil.getCol("stock","daily_deal")
+
+
+def  getGdpDate(year, quarter):
+    if quarter == 0:
+        return str(year-1)+"-12-01"
+    if quarter ==4:
+        return str(year)+'-12-01'
+    return str(year)+'-0'+ str(quarter*3)+'-01'
 
 gdpCursor = gdpCol.find({"date":{"$gt":"2019-03-01"}}).sort("date",1)
 gdpDict={}
@@ -16,31 +24,30 @@ pipeline = [
     {'$group': {'_id': "$date", 'market_amount': {'$sum': "$market_amount"},'deal_amount': {'$sum': "$deal_amount"}}},
     {"$sort": {"_id": 1}}
 ]
+date=[]
+amountGdpRate=[]
 dailyCursor = dailyCol.aggregate(pipeline)
 for dailyData in dailyCursor:
-    print(dailyData)
+    date.append(dailyData["_id"])
+    dt = datetime.strptime(dailyData["_id"], "%Y%m%d") 
+    quarter = int(dt.month/3)
+    gdp = gdpDict[getGdpDate(dt.year,quarter)]['gdp']
+    amountGdpRate.append(dailyData['market_amount']/gdp)
 
 import pyecharts.options as opts
 from pyecharts.charts import Line
-
-# 写入数据
-years = ["1995", "1996", "1997", "1998", "1999", "2000",
-         "2001", "2002", "2003", "2004", "2005", "2006",
-         "2007", "2008", "2009"]
-Postage = [0.32, 0.32, 0.32, 0.32, 0.33, 0.33, 0.34,
-           0.37, 0.37, 0.37, 0.37, 0.39, 0.41, 0.42, 0.44]
 
 (
     Line()
     # 全局配置
     .set_global_opts()
     # x轴
-    .add_xaxis(xaxis_data=years)
+    .add_xaxis(xaxis_data=date)
     # y轴
     .add_yaxis(
         series_name="",         # 数据所属集合的名称
-        y_axis=Postage,            # y轴数据
+        y_axis=amountGdpRate,            # y轴数据
     )
     # 输出为HTML文件
-    # .render_notebook()
+    .render()
 )
